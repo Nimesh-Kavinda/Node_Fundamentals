d8e59f36424f600c4b350abbd8e896444c6f7bdb
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-09-10 00:22:29 +0530

    Start the Node Learning Journy

---
+++ 01.Hello_World/index.js
@@ -0,0 +1,11 @@
+console.log("Hello, Node Js!");
+
+const array = [1, 2, 3, 4, 5];
+
+console.log(array, 'array');
+
+setTimeout(() => {
+  console.log('Timeout executed after 2 seconds');
+}, 2000);
+
+console.log('End of script');
